27919945a01f7119d86e45c21d6317188efa5b86
PeekMe01 <ralphdaher6@gmail.com>
2023-06-14 16:29:16 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/header/Header'
+import PhotoSection from './components/photoSection/PhotoSection'
+import AboutMe from './components/aboutMe/AboutMe'
+import Skills from './components/skills/Skills'
+import Portfolio from './components/portfolio/Portfolio'
+import ContactMe from './components/contactMe/ContactMe'
+import Footer from './components/footer/Footer'
+import Navigation from './components/nav/Navigation'
+
+
+const App = () => {
+    return(
+        <>
+        <Header />
+        <Navigation />
+        <PhotoSection />
+        <AboutMe />
+        <Skills />
+        <Portfolio />
+        <ContactMe />
+        <Footer />
+        </>
+    )
+}
+
+export default App
