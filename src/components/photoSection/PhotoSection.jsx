dd7bd9791cd53a438ee5878a490f1641490f854e
PeekMe01 <ralphdaher6@gmail.com>
2023-06-14 16:29:52 +0300

    Create PhotoSection.jsx

---
+++ src/components/photoSection/PhotoSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './photoSection.css'
+
+const PhotoSection = () => {
+  return (
+    <div>
+      Photo Section
+    </div>
+  )
+}
+
+export default PhotoSection
