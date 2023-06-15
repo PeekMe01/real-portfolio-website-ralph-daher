405a186767d2c11846fa25218e527657e650acf3
PeekMe01 <ralphdaher6@gmail.com>
2023-06-15 20:00:58 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './header.css'
+import ME from '../../assets/Profile Pic.jpg'
+import CTA from './CTA'
+import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <Fade>
+    <header className='container'>
+      <div className="header__container">
+        <div className="titles">
+          <h1 className='light-yellow'>Ralph Daher</h1>
+          <small className='text-light'>University Student</small>
+        </div>
+        <div>
+          <img src={ME} alt="Profile Pic" className='profilePicture'/>
+        </div>
+      </div>
+      <hr/>
+      <Zoom><CTA/></Zoom>
+    </header>
+    </Fade>
   )
 }
 
