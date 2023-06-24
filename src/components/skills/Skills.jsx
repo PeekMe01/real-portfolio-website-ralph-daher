2ed3fe61f690b20f841222c3fdf4603fe46d3897
PeekMe01 <ralphdaher6@gmail.com>
2023-06-24 11:37:20 +0300

    Update Skills.jsx
    no need to thank me

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './skills.css'
 import Skill from './Skill'
+import Zoom from 'react-reveal/Zoom'
 
 const Skills = () => {
+
+  const [skill, setSkill] = useState([{skill_id: 0, skill_name: '', skill_level: 0}]);
+
+  useEffect(() => {
+    // Fetch data from the PHP file
+    fetch('http://localhost:8081/skills')
+      .then(response => response.json())
+      .then(result => {
+        setSkill(result);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <section id='skills'>
-      <div className='container container__skills'>
-        <div className="skills__content">
+      <div className='container container__skills skillContainer'>
+      <div className='top__text'>
+        <Zoom>
+          <h5 className='text-light'>What Skills I Have</h5>
+          <h2>My Skills</h2>
+        </Zoom>
+      </div>
+
+        {/* <div className="skills__content">
           <div className="leftTable">
             <Skill skill='Java' level={4}/>
             <Skill skill='C++' level={4}/>
@@ -24,6 +47,15 @@ const Skills = () => {
             <Skill skill='GitHub' level={4}/>
             <Skill skill='UI-UX' level={4}/>
             <Skill skill='Problem Solving' level={4}/>
+          </div>
+        </div> */}
+
+        <div className="skills__content">
+          <div className="leftTable">
+              {skill.slice(0,(skill.length/2)+1).map((object, i) => <Skill id={object.skill_id} skill={object.skill_name} level={object.skill_level} />)}
+          </div>
+          <div className="rightTable">
+              {skill.slice((skill.length/2)+1,skill.length).map((object, i) => <Skill id={object.skill_id} skill={object.skill_name} level={object.skill_level} />)}
           </div>
         </div>
       </div>
